536bfd9d88304aa1e6b598fa451cdfaf9e48dc81
Megan <okerlund.megan@gmail.com>
2019-06-19 19:53:44 -0700

    More minor changes

--- BigMartRedo.py
+++ BigMartRedo.py
@@ -2,8 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from scipy.stats import norm
+from scipy.stats import expon
+from scipy.stats import binom
+from scipy.stats import poisson
 
-df = pd.read_csv('bigmartsalestest.csv')
+df = pd.read_csv('bigmartsalestrain.csv')
 #train = pd.read_csv('bigmartsalestrain.csv')
 
 #df = pd.concat([train, test], ignore_index=True, sort=True)
@@ -17,6 +21,7 @@ df.isnull().sum()
 df.columns
 
 ##Target Variable: Item_Outlet_Sales
+##
 
 #Removing null values
 avg_weight = df.Item_Weight.mean()
@@ -28,4 +33,9 @@ missing = df.Outlet_Size.isnull()
 Outlet_Size_missing = df.Outlet_Size.isnull()
 df.loc[Outlet_Size_missing, 'Outlet_Size'] = missing
 
-df.Item_Fat_Content.rename(columns={'LF':'Low Fat', 'reg':'Regular', 'low fat':'Low Fat'}, inplace=True)
+df.Item_Fat_Content = df.Item_Fat_Content.replace({'LF':'Low Fat', 'reg':'Regular', 'low fat':'Low Fat'})
+
+#Outlet sizes are medium, true, small and high **Needs to be changed, doesn't make sense
+#Outlet_types are Grocery store, supermarkets 1, 2, and 3, this is fine for now
+
+ 
